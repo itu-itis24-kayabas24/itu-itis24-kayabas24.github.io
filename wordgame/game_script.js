fbde3203013d42fe99feb3ecd3b804d9782d380f
itu-itis24-kayabas24 <kayabas24@itu.edu.tr>
2024-12-30 23:18:17 +0300

    Update game_script.js

---
+++ wordgame/game_script.js
@@ -0,0 +1,15 @@
+/* Variables*/
+
+const wanted_word= "STOCK"
+let lives_count = 3
+let tried_letters = []
+let score = 0
+
+restart_button = document.getElementById("refresh")
+
+remaining_lives = document.getElementById("remaining_lives")
+
+word =  document.getElementById("word")
+
+
+    
